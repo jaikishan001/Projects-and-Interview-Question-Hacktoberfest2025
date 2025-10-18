11489c67b091d98ea47f7e5fe336d3f1458dddda
jaikishan <157561388+jaikishan001@users.noreply.github.com>
2025-10-18 14:46:19 +0530

    Create MaxSubarraySum.java

---
+++ MaxSubarraySum.java
@@ -0,0 +1,12 @@
+public class MaxSubarraySum {
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0], currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
